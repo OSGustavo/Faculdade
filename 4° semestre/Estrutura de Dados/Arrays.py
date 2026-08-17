06d03bf557b6076ccfc125de057c809535a3e611
Gustavo Santos <gustavo.os170205@gmail.com>
2026-08-17 15:19:59 -0300

    Feat: Adicionado a função de inserir

--- 4° semestre/Estrutura de Dados/Arrays.py
+++ 4° semestre/Estrutura de Dados/Arrays.py
@@ -5,11 +5,22 @@ def inicializar_array(tamanho):
     array = [0.0] * tamanho
     return array
 
+def inserir(array, indice, valor):
+    if indice < 0 or indice >= len(array):
+        return False
 
-tamanho = int(input("Digite o tamanho do vetor: "))
+    array[indice] = float(valor)
+    return True
+
+
+tamanho = int(input("Informe o tamanho do vetor: "))
 vetor = inicializar_array(tamanho)
 
 if vetor is None:
     print("O tamanho deve ser maior que zero.")
 else:
-    print("Vetor iniciado:", vetor)
+    for i in range(tamanho):
+        valor = float(input(f"Informe o valor da posição {i}: "))
+        inserir(vetor, i, valor)
+
+    print("Vetor preenchido:", vetor)
